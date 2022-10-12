40ec239a6562003a51b30da268d18caa38be537a
Bruna Namie <bruna.yamasaki123@gmail.com>
2022-10-12 14:48:23 -0300

    update re-bert code

--- data_utils.py
+++ data_utils.py
@@ -79,37 +79,6 @@ def pad_and_truncate(sequence, maxlen, dtype='int64', padding='post', truncating
     return x
 
 
-class Tokenizer(object):
-    def __init__(self, max_seq_len, lower=True):
-        self.lower = lower
-        self.max_seq_len = max_seq_len
-        self.word2idx = {}
-        self.idx2word = {}
-        self.idx = 1
-
-    def fit_on_text(self, text):
-        if self.lower:
-            text = text.lower()
-        words = text.split()
-        for word in words:
-            if word not in self.word2idx:
-                self.word2idx[word] = self.idx
-                self.idx2word[self.idx] = word
-                self.idx += 1
-
-    def text_to_sequence(self, text, reverse=False, padding='post', truncating='post'):
-        if self.lower:
-            text = text.lower()
-        words = text.split()
-        unknownidx = len(self.word2idx)+1
-        sequence = [self.word2idx[w] if w in self.word2idx else unknownidx for w in words]
-        if len(sequence) == 0:
-            sequence = [0]
-        if reverse:
-            sequence = sequence[::-1]
-        return pad_and_truncate(sequence, self.max_seq_len, padding=padding, truncating=truncating)
-
-
 class Tokenizer4Bert:
     def __init__(self, max_seq_len, pretrained_bert_name):
         self.tokenizer = BertTokenizer.from_pretrained(pretrained_bert_name)
